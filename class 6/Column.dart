6fbcbb6544d23514268668584ef93e391ad4bcca
AsifImtiazShazid <82169020+AsifImtiazShazid@users.noreply.github.com>
2021-09-13 16:18:57 +0600

    Create Column.dart

---
+++ class 6/Column.dart
@@ -0,0 +1,59 @@
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('MyApp'),
+      ),
+      body: Container(
+        child: Center(
+            child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    print('Clicked');
+                  },
+                  child: Text(
+                    'Click Me',
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    print('Type');
+                  },
+                  child: Text('Click')),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
