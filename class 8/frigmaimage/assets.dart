89b5ca04a554bae3c985e51a66b5e7b49b28ccb1
AsifImtiazShazid <82169020+AsifImtiazShazid@users.noreply.github.com>
2021-09-18 15:47:18 +0600

    Create assets

---
+++ class 8/frigmaimage/assets.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Image.asset('assets/images/splash_image_2.png')),
+    );
+  }
+}
